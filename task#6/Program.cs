baed5c060ebab87987356234d9a499b7399c0e84
Alex <asdsu@example.com>
2022-10-06 19:07:31 +0300

    Задача #6

---
+++ task#6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите  число");
+double a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+    Console.WriteLine("Четное и делится на 2 без остатка");
+}    
+else
+{
+    Console.WriteLine("Не явлчется четным");
+}    
+  
