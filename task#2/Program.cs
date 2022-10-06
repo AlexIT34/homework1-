74a928936ec58f1a36821ebb8b1bf61f553cdc95
Alex <asdsu@example.com>
2022-10-06 13:35:43 +0300

    Задача №2

---
+++ task#2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Значение a является max");
+}    
+else
+{
+    Console.WriteLine("Значение b является max");
+}    
