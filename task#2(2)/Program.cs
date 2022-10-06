f8d834e538c8119a5e6275b7fc8b4353995ca73c
Alex <asdsu@example.com>
2022-10-06 13:58:55 +0300

    Задача #2, 2 вариант

---
+++ task#2(2)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
